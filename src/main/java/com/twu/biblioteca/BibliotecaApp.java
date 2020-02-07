f4e85f0c2e2ba61585f6275ae822acac375b2cea
Md Shahid <md.shahid@thoughtworks.com>
2020-02-07 18:53:11 +0530

    Requirement 1.2 |Add. functionality to display all books for library

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -1,14 +1,26 @@
 package com.twu.biblioteca;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class BibliotecaApp {
+    private String welcomeMessage = "Welcome to Biblioteca. You one-stop-shop for great book titles in Bangalore!";
+    private List<String> bookList = asList("1. Mathematics", "2. Physics", "3. Astrophysics");
 
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
         bibliotecaApp.start();
+        bibliotecaApp.displayAllBooks();
     }
 
     public void start() {
-        System.out.println("Welcome to Biblioteca. You one-stop-shop for great book titles in Bangalore!");
+        System.out.println(welcomeMessage);
+    }
+
+    public void displayAllBooks() {
+        for (String book : bookList) System.out.println(book);
+
     }
 
 }
--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,19 +1,30 @@
 package com.twu.biblioteca;
-
 import org.junit.jupiter.api.Test;
-
 import java.io.PrintStream;
-
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 class BibliotecaAppTest {
 
     @Test
-    public void testShouldCheckThatAWelcomeMessageIsPrinting() {
+    public void testShouldPrintTheWelcomeMessage() {
         PrintStream printStream = mock(PrintStream.class);
         System.setOut(printStream);
         BibliotecaApp.main(new String[]{});
         verify(printStream).println("Welcome to Biblioteca. You one-stop-shop for great book titles in Bangalore!");
+    }
+
+    @Test
+    public void testShouldBeAbleToPrintAllAvailableBooks() {
+        PrintStream printStream = mock(PrintStream.class);
+        System.setOut(printStream);
+
+        BibliotecaApp.main(new String[]{});
+
+        verify(printStream).println("Welcome to Biblioteca. You one-stop-shop for great book titles in Bangalore!");
+        verify(printStream).println("1. Mathematics");
+        verify(printStream).println("2. Physics");
+        verify(printStream).println("3. Astrophysics");
+
     }
 }

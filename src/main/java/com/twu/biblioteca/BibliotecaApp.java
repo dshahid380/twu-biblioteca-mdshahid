025e3468e9af89a75962ccbf9f3ea96a7b83ec4d
Md Shahid <md.shahid@thoughtworks.com>
2020-02-10 08:29:36 +0530

    Requirent 1.6 |Added Quit application functionality and Extracted display all book functionality to BookShelf class and showMenu functionality is moved to separate class menu

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -1,52 +1,27 @@
 package com.twu.biblioteca;
+
 import java.util.*;
 
 public class BibliotecaApp {
-    private String welcomeMessage = "Welcome to Biblioteca. You one-stop-shop for great book titles in Bangalore!";
-    private List<Book> bookList = Arrays.asList(new Book("Book1", "Author1", "1997"),
-                                                new Book("Book2", "Author2", "1998"));
-    private HashMap<Integer, String> menuList = new HashMap<>();
-    private int userInput;
+    private String welcomeMessage;
+
+    private int input;
 
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
-        bibliotecaApp.menuList.put(1,"List of Books");
-
-        bibliotecaApp.start();
-        bibliotecaApp.showMenu();
-
-        if(bibliotecaApp.getUserInput().equals("1"))
-            bibliotecaApp.displayAllBooks();
-        else
-            System.out.println("Please select a valid option!");
-
-    }
-
-    private void start() {
-        System.out.println(welcomeMessage);
-    }
-
-    private void displayAllBooks() {
-        for (Book book : bookList) {
-            System.out.print(book.getBookName());
-            System.out.print(" ");
-            System.out.print(book.getAuthorName());
-            System.out.print(" ");
-            System.out.print(book.getYear());
-            System.out.println("");
-        }
-    }
-
-    private void showMenu(){
-        for(Map.Entry<Integer, String> entry : menuList.entrySet())
-            System.out.print(entry.getKey() + ". " + entry.getValue());
-        System.out.println("\nPlease choose an option : ");
-    }
-
-    public String getUserInput(){
+        Menu menu = new Menu();
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        return s;
+        bibliotecaApp.start();
+        do {
+            menu.show();
+            bibliotecaApp.input = scanner.nextInt();
+            menu.performOperation(bibliotecaApp.input);
+        } while (bibliotecaApp.input != 4);
+    }
+
+    public void start() {
+        welcomeMessage = "Welcome to Biblioteca. You one-stop-shop for great book titles in Bangalore!";
+        System.out.println(welcomeMessage);
     }
 
 }
--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,10 +1,9 @@
 package com.twu.biblioteca;
+
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.Scanner;
 
 import static org.mockito.Mockito.*;
 
@@ -14,9 +13,11 @@ class BibliotecaAppTest {
     public void testShouldPrintTheWelcomeMessage() {
         PrintStream printStream = mock(PrintStream.class);
         System.setOut(printStream);
-        String input = "1";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        System.setIn(new ByteArrayInputStream("1".getBytes()));
+        System.setIn(new ByteArrayInputStream("4".getBytes()));
+
         BibliotecaApp.main(new String[]{});
+
         verify(printStream).println("Welcome to Biblioteca. You one-stop-shop for great book titles in Bangalore!");
     }
 
@@ -24,13 +25,13 @@ class BibliotecaAppTest {
     public void testShouldBeAbleToPrintAllAvailableBooks() {
         PrintStream printStream = mock(PrintStream.class);
         System.setOut(printStream);
-        String input = "1";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        System.setIn(new ByteArrayInputStream("1\n4".getBytes()));
 
         BibliotecaApp.main(new String[]{});
 
         verify(printStream).print("Book1");
         verify(printStream).print("Book2");
+        verify(printStream).println("Thank you!");
 
     }
 
@@ -38,8 +39,7 @@ class BibliotecaAppTest {
     void testShouldPrintAllBooksWithTheirAuthorsNameAndYear() {
         PrintStream printStream = mock(PrintStream.class);
         System.setOut(printStream);
-        String input = "1";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        System.setIn(new ByteArrayInputStream("1\n4".getBytes()));
 
         BibliotecaApp.main(new String[]{});
 
@@ -56,29 +56,39 @@ class BibliotecaAppTest {
         PrintStream printStream = mock(PrintStream.class);
         System.setOut(printStream);
 
-        String input = "1";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        System.setIn(new ByteArrayInputStream("1\n4".getBytes()));
         BibliotecaApp.main(new String[]{});
 
-        verify(printStream).print("1. List of Books");
+        verify(printStream, times(2)).print("1. List of Books");
         verify(printStream).print("Book1");
         verify(printStream).print("Author1");
         verify(printStream).print("1997");
         verify(printStream).print("Book2");
         verify(printStream).print("Author2");
         verify(printStream).print("1998");
-
     }
 
     @Test
     void testShouldPrintInvalidOptionIfUserChooseInvalidOption() {
         PrintStream printStream = mock(PrintStream.class);
         System.setOut(printStream);
+        System.setIn(new ByteArrayInputStream("6\n4".getBytes()));
 
-        String input = "2";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
         BibliotecaApp.main(new String[]{});
 
-        verify(printStream).println("Please select a valid option!");
+        verify(printStream,times(1)).println("Please select a valid option!");
     }
+
+    @Test
+    void testShouldQuitTheApplicationWhenUserOptToQuit() {
+        PrintStream printStream = mock(PrintStream.class);
+        System.setOut(printStream);
+
+        System.setIn(new ByteArrayInputStream("4".getBytes()));
+
+        BibliotecaApp.main(new String[]{});
+
+        verify(printStream).println("Thank you!");
+    }
+
 }
